9491f2342bd3cd8890de5a07e4b63b2990190e49
Rémi Eisack <remi.eisack@gmail.com>
2019-06-22 15:55:46 +0200

    Add players names

--- app/src/main/java/rek/gofscoreboard/MainActivity.kt
+++ app/src/main/java/rek/gofscoreboard/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.RadioButton
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +15,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun startGame(view: View) {
-        val scoreboardIntent = Intent(this, ScoreboardActivity::class.java)
+    private fun getNbPlayers(): Int {
         val checkedNbPlayers: RadioButton = findViewById(radioGroupNbPlayers.checkedRadioButtonId)
-        val nbPlayers = Integer.parseInt(checkedNbPlayers.text.toString())
+        return Integer.parseInt(checkedNbPlayers.text.toString())
+    }
+
+    fun onStartGame(view: View) {
+        val scoreboardIntent = Intent(this, ScoreboardActivity::class.java)
+        val nbPlayers = getNbPlayers()
 
         scoreboardIntent.putExtra(ScoreboardActivity.NB_PLAYERS, nbPlayers)
-
         startActivity(scoreboardIntent)
     }
+
+    fun onNbPlayersChanged(view: View) {
+        val nbPlayers = getNbPlayers()
+        val playerFourVisible = if (nbPlayers == 3) View.INVISIBLE else View.VISIBLE
+
+        labelPlayerFourName.visibility = playerFourVisible
+        editPlayerFourName.visibility = playerFourVisible
+    }
+
+
 }
--- app/src/main/java/rek/gofscoreboard/ScoreboardActivity.kt
+++ app/src/main/java/rek/gofscoreboard/ScoreboardActivity.kt
@@ -8,13 +8,13 @@ class ScoreboardActivity : AppCompatActivity() {
 
     companion object {
         const val NB_PLAYERS = "NB_PLAYERS"
+        var nbPlayerValue: Int? = null
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scoreboard)
 
-        val nbPlayers = intent.getIntExtra(NB_PLAYERS, 0)
-        labelTest.text = nbPlayers.toString()
+        nbPlayerValue = intent.getIntExtra(NB_PLAYERS, 0)
     }
 }
