88fd1e1b7a043f872cb0c3824b032e13499a1b69
Rémi Eisack <remi.eisack@gmail.com>
2025-07-18 23:01:00 +0200

    Refactoring

--- app/src/main/java/rek/gofscoreboard/MainViewModel.kt
+++ app/src/main/java/rek/gofscoreboard/MainViewModel.kt
@@ -27,24 +27,25 @@ class MainViewModel : ViewModel() {
         playerFourVisibility.value = if (nbPlayers == 3) View.INVISIBLE else View.VISIBLE
     }
 
-    fun isEveryPlayerNameFilled(): Boolean =
-        listOfNotNull(
-            playerOneName.value,
-            playerTwoName.value,
-            playerThreeName.value,
-            if (nbPlayers == 4) playerFourName.value else null
-        ).all { it.isNotEmpty() }
+    fun isEveryPlayerNameFilled(): Boolean = getPlayerNameList().all { it.isNotEmpty() }
 
     fun isEveryPlayerNameDifferent(): Boolean {
-        val names = listOf(playerOneName.value, playerTwoName.value, playerThreeName.value, playerFourName.value)
+        val names = getPlayerNameList()
 
         // Check is case-insensitive
-        val lowered = names.map { it?.lowercase() }
+        val lowered = names.map { it.lowercase() }
 
         // Putting them in a Set, which removes duplicates.
         // if size is the same in the set and in original list, every player name is different
         return lowered.size == lowered.toSet().size
     }
+
+    private fun getPlayerNameList() = listOfNotNull(
+        playerOneName.value,
+        playerTwoName.value,
+        playerThreeName.value,
+        if (nbPlayers == 4) playerFourName.value else null
+    )
 
     fun delayedClearPlayersNames() {
         Handler(Looper.myLooper()!!).postDelayed({
